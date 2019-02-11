271251f54fa4ecc15c9de82f03ec2df093a1191b
Zamorcs <threepeople@gmail.com>
2019-02-11 16:41:58 -0300

    Cascara de proyecto
    ++primer commit con proyecto generado en springboot initializr

---
+++ src/main/java/com/bindot/runap/RunapApplication.java
@@ -0,0 +1,14 @@
+package com.bindot.runap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunapApplication.class, args);
+	}
+
+}
+
